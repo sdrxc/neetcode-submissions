66db9bd6249955511e6ef34a589879bc95b2d2f1
Soumya Deep Roy <67396197+sdrxc@users.noreply.github.com>
2026-06-09 02:37:46 +0530

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+
+        // BRUTE FORCE METHOD
+        int n = nums.size();
+        for(int i=1;i<=n+1;i++)
+        {
+            auto it = find(nums.begin(), nums.end(), i);
+            if(it==nums.end())
+                return i;
+            
+        }
+
+        return 0;
+    }
+};
