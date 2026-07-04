6edf8456d5d0ff079a168c4e8712ddd138881067
Soumya Deep Roy <67396197+sdrxc@users.noreply.github.com>
2026-07-04 15:20:35 +0530

    Add: find-duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            int idx = abs(nums[i])-1;
+
+            if(nums[idx]<0) return idx+1;
+
+            nums[idx]*=-1; 
+        }
+
+        return -1;
+    }
+};
