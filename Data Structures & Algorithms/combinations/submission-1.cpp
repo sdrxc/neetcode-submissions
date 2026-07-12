ee0d3860c1c6ff4f7ccbbbff8031e407778bca6e
Soumya Deep Roy <67396197+sdrxc@users.noreply.github.com>
2026-07-12 21:03:42 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private: 
+    vector<vector<int>> ans;
+    vector<int> curr;
+public:
+    void findCombinations(int start, int n, int k)
+    {
+        if(curr.size() == k)
+        {
+            ans.push_back(curr);
+            return;
+        }
+
+        for(int i = start; i <= n; i++)
+        {
+            curr.push_back(i);
+
+            findCombinations(i + 1, n, k);
+
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        findCombinations(1, n, k);
+        return ans;    
+    }
+};
