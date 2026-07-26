63905ce231df25b23de6228e1c9f592a79af252c
Soumya Deep Roy <67396197+sdrxc@users.noreply.github.com>
2026-07-26 21:03:11 +0530

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        queue<pair<int, int>> q;    // multi source BFS     
+        const int INF = INT_MAX;
+         // Add all gates
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 0) {
+                    q.push({r, c});
+                }
+            }
+        }
+
+        vector<pair<int, int>> dirs = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        };   
+
+        while(!q.empty())
+        {
+            auto [r,c] = q.front();
+            q.pop();
+
+            for(auto [dr,dc]: dirs)
+            {
+                 int nr = r+dr;
+                 int nc = c+dc;
+
+                if(nr < 0 || nc < 0 || nr >= rows || nc >= cols)
+                    continue;
+
+                 if(grid[nr][nc] != INF)
+                    continue;
+
+                 grid[nr][nc] = grid[r][c] + 1;
+                q.push({nr, nc});                                            
+            }
+        }
+
+    }
+};
