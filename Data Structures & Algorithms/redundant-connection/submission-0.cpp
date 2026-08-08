60f219a8288844f6ce4899a7309d57160ee54177
Soumya Deep Roy <67396197+sdrxc@users.noreply.github.com>
2026-08-08 22:28:52 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> parent;
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]); // path compression
+        return parent[x];
+    }
+
+    bool unite(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa == pb)
+            return false; // cycle detected
+
+        parent[pa] = pb;
+        return true;
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+
+        int n = edges.size();
+        parent.resize(n + 1);
+
+        for (int i = 1; i <= n; i++)
+            parent[i] = i;
+
+        for (auto &e : edges) {
+            if (!unite(e[0], e[1])) {
+                return e; // this edge creates cycle
+            }
+        }
+
+        return {};
+    }
+};
