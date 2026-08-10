6d20c74949d40904a610b375b27009c5b1fb5054
Soumya Deep Roy <67396197+sdrxc@users.noreply.github.com>
2026-08-11 03:29:56 +0530

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+         
+        vector<vector<int>> res;
+        int i = 0, n = intervals.size();
+
+        // place the intervals smaller than new intervals into the res
+        while(i<n && intervals[i][1] < newInterval[0])
+        {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
+        // 2. Merge all overlapping intervals with newInterval
+        while(i < n && intervals[i][0] <= newInterval[1])
+        {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        // finally pushing the updated new interval
+        res.push_back(newInterval);
+
+        // residual intervals
+        while(i<n)
+        {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
+        return res;
+    }
+};
