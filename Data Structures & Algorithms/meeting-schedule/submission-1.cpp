470ba74ffdedb99986a1f417223ecfb7f1cc509f
Soumya Deep Roy <67396197+sdrxc@users.noreply.github.com>
2026-08-11 16:25:22 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.empty()) return true;
+
+        // Sort intervals by their start time using a lambda comparator
+        sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b){
+            return a.start < b.start;
+        });
+
+        // Check for overlaps
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+
+        return true;
+
+        if(intervals.empty()) return true;
+
+
+    }
+};
